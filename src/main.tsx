50c679754da039106e459d4d2f9b35268b78211c
YanTano <101397014+YanTano@users.noreply.github.com>
2026-08-03 20:15:16 +0800

    Add files via upload

---
+++ src/main.tsx
@@ -0,0 +1,19 @@
+/**
+ * @license
+ * SPDX-License-Identifier: Apache-2.0
+*/
+
+import {StrictMode} from 'react';
+import {createRoot} from 'react-dom/client';
+import App from './App.tsx';
+import { ErrorBoundary } from './components/ErrorBoundary.tsx';
+import './index.css';
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
+  </StrictMode>,
+);
+
